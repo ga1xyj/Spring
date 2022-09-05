52898b67bbc6a644cab18051a2f80aa12d137523
admin <admin@DESKTOP-PDK7U4E>
2022-09-05 17:45:52 +0900

    0905

--- ex01/src/main/java/com/yedam/java/emp/service/EmpVO.java
+++ ex01/src/main/java/com/yedam/java/emp/service/EmpVO.java
@@ -5,10 +5,14 @@ import lombok.Data;
 @Data
 public class EmpVO {
 	String employeeId;
-	   String firstName;
-	   String lastName;
-	   String email;
-	   String hireDate;
-	   String jobId;
-	   String salary;
+   String firstName;
+   String lastName;
+   String email;
+   String hireDate;
+   String jobId;
+   String salary;
+   String commissionPct;
+   String managerId;
+   String departmentId;
+   String phoneNumber;
 }
